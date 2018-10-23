acb10daee8d8831353c665e8077d867bc420a313
Jeffrey Chen <jeffreychen2016@gmail.com>
2018-10-23 13:38:49 -0500

    modified the getLinksForDownladingDocx function to download correct file

--- process.php
+++ process.php
@@ -1,24 +1,4 @@
 <?php
-	// error_reporting(0);
-	// function openPDF(){
-	// 	if (isset($_GET['ET_Template']))
-	// 	{
-	// 		$file = array_keys($_GET)[1];
-	// 		$filename = $file . '.txt';		
-	// 		echo "<div class='iframe'>Your file is ready to be downloaded here:<br>
-	// 						<a href='Default_Export_Templates/" . $filename . "' download>". $filename . "</a>
-	// 					</div>";
-	// 	}
-	// 	else
-	// 	{
-	// 		$file = key($_GET);
-	// 		$filename = $file . '.pdf';
-	// 		echo 'file name:' . $filename;
-	// 		// echo '<iframe class="iframe" src="docs/'. $directoryName . '/' . $filename . '"' . '></iframe>';
-	// 		echo '<iframe class="iframe" src="docs/'. 'default export template' . '/' . $filename . '"' . '></iframe>';
-	// 	};
-	// };
-
 	function openPDF($direcotry,$filename){
 		// the space in the filename is changed to _ (underscore) for some reason
 		// so in order to natch the file name, replace the _ with empty
@@ -26,18 +6,15 @@
 	};
 
 	function getLinksForDownladingDocx(){
-		if (isset($_GET['ET_Template']))
+		if (isset($_GET['directoryName']))
 		{
-			$file = array_keys($_GET)[1]; //second key in the url
-			$filename = $file . '.txt';
-			echo '"Default_Export_Templates/' . $filename . '"';			
-		}
-		else
-		{
-			$file = key($_GET);
-			$filename = $file . '.docx';
-			echo '"docs/' . $filename . '"';
-		};
+			$direcotryName = $_GET['directoryName'];
+			$file = $_GET['fileName'];
+			$fileName = $file . '.pdf';
+			echo '"docs\\' . $direcotryName . '\\' . $fileName . '"';
+		} else {
+			echo '""';
+		}; 
 	};
 
 	function readDirecotries(){
@@ -85,17 +62,21 @@
 		return $domString;
 	};
 
-	if (isset($_POST['submit'])) 
-	{	
-		$target_dir = 'C:\\xampp\htdocs\\Comdata_Prod\\docs\\' . $_POST['file_destination_dropdown'] . '\\';
-		$target_file = $target_dir . basename($_FILES["the_file"]["name"]);
-		$message = 0;
-
-    if (move_uploaded_file($_FILES["the_file"]["tmp_name"], $target_file)) {
-			$message = 1;
-		} else {
+	function uploadFile(){
+		if (isset($_POST['submit'])) 
+		{	
+			$target_dir = 'C:\\xampp\htdocs\\Comdata_Prod\\docs\\' . $_POST['file_destination_dropdown'] . '\\';
+			$target_file = $target_dir . basename($_FILES["the_file"]["name"]);
 			$message = 0;
+	
+			if (move_uploaded_file($_FILES["the_file"]["tmp_name"], $target_file)) {
+				$message = 1;
+			} else {
+				$message = 0;
+			};
+			header('Location: index.php?msg=' . $message); 
 		};
-		header('Location: index.php?msg=' . $message); 
 	};
+
+	uploadFile();
 ?>
